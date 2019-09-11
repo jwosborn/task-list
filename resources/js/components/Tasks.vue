f25c10d7080130c3d45130dfb27a945c44a09a0d
kelseymegan92 <kelseymegan92@Kelseys-MacBook-Pro.local>
2019-09-11 12:10:46 -0400

    layout styles

--- resources/js/components/Tasks.vue
+++ resources/js/components/Tasks.vue
@@ -1,31 +1,42 @@
 <template>
-  <div>
-    <h3>Daily Tasks</h3>
-    <div :key="task" class="task-wrapper" v-for="task in tasks">
-      <div class="task" v-if="!task.isDone && !task.weekly">
-        <div class="option">{{ task.value }}</div>
-        <div class="complete" @click="task.isDone = !task.isDone">DONE</div>
+  <div class="task-list-wrapper">
+    <div class="task-wrapper">
+      <h3>Daily Tasks</h3>
+      <div :key="task" v-for="task in tasks">
+        <div class="task" v-if="!task.isDone && !task.weekly">
+          <div class="option">{{ task.value }}</div>
+          <div class="complete" @click="task.isDone = !task.isDone">DONE</div>
+        </div>
       </div>
     </div>
-    <h3>Weekly Tasks</h3>
-    <div :key="task" class="task-wrapper" v-for="task in tasks">
-      <div class="task" v-if="task.weekly && !task.isDone">
-        <div class="option">{{ task.value }}</div>
-        <div class="complete" @click="task.isDone = !task.isDone">DONE</div>
+
+    <div class="task-wrapper">
+      <h3>Weekly Tasks</h3>
+      <div :key="task" v-for="task in tasks">
+        <div class="task" v-if="task.weekly && !task.isDone">
+          <div class="option">{{ task.value }}</div>
+          <div class="complete" @click="task.isDone = !task.isDone">DONE</div>
+        </div>
       </div>
     </div>
-    <h3>Completed Daily Tasks</h3>
-    <div :key="task" class="task-wrapper" v-for="task in tasks">
-      <div class="task" v-if="task.isDone && !task.weekly">
-        <div class="option">{{ task.value }}</div>
-        <div class="complete" @click="task.isDone = !task.isDone">DONE</div>
+
+    <div class="task-wrapper">
+      <h3>Completed Daily Tasks</h3>
+      <div :key="task" v-for="task in tasks">
+        <div class="task" v-if="task.isDone && !task.weekly">
+          <div class="option">{{ task.value }}</div>
+          <div class="complete" @click="task.isDone = !task.isDone">DONE</div>
+        </div>
       </div>
     </div>
-    <h3>Completed Weekly Tasks</h3>
-    <div :key="task" class="task-wrapper" v-for="task in tasks">
-      <div class="task" v-if="task.isDone && task.weekly">
-        <div class="option">{{ task.value }}</div>
-        <div class="complete" @click="task.isDone = !task.isDone">DONE</div>
+
+    <div class="task-wrapper">
+      <h3>Completed Weekly Tasks</h3>
+      <div :key="task" v-for="task in tasks">
+        <div class="task" v-if="task.isDone && task.weekly">
+          <div class="option">{{ task.value }}</div>
+          <div class="complete" @click="task.isDone = !task.isDone">DONE</div>
+        </div>
       </div>
     </div>
   </div>
@@ -87,25 +98,35 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 15px auto;
+  margin: 0 auto 0.75em;
   background: #42b983;
   color: #f5f5f5;
-  width: 50vw;
+  width: 12vw;
+  height: 55px;
+}
+.task-list-wrapper {
+  display: flex;
+  flex-direction: row;
+  margin: 0 1em;
 }
 .task {
   display: flex;
   flex-direction: column;
-  margin: auto;
+  margin: 5px auto;
   background-color: lightgray;
-  width: 50vw;
+  width: 150px;
 }
-.tasks {
+.task-wrapper {
   display: flex;
+  flex-direction: column;
+  margin: 1em;
+  flex-grow: 1;
 }
 .complete {
   margin: auto;
+  margin-top: 0.25em;
   margin-bottom: 0.5em;
-  height: 1e.25m;
+  height: 1.5em;
   width: 50px;
   background-color: #2c3e50;
   color: white;
